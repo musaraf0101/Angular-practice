f3520f70ed04e26ed1beb1ff74d0ecfa7be28658
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-05-24 21:18:06 +0530

    first project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    DashbordComponent
+    DashbordComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
